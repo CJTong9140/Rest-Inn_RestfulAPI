fd7fe7b0934abfa925ec274cd9ecbb471cd5232e
CJ Jingren Tong <cj.tong93@gmail.com>
2022-02-03 17:13:20 -0500

    Testing mongoDB connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express"); 
+const mongoose = require("mongoose"); 
 
 if(process.env.NODE_ENV!=="production"){
     require("dotenv").config({path:"config/keys.env"}); 
@@ -8,4 +9,12 @@ const app = express();
 
 app.listen(process.env.PORT, async()=>{
     console.log(`Web Server is up and running on PORT ${process.env.PORT}!`); 
+    
+    try{
+        await mongoose.connect(process.env.MONGODB_QUERY_STRING); 
+        console.log(`You are now connected to MongoDB`); 
+    }
+    catch(err){
+        console.log(`Error: ${err}`);
+    }
 })
