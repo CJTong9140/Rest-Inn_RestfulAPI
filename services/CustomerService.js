745de096c5a285b89906e9a63ea8a0436a28a750
CJ Jingren Tong <cj.tong93@gmail.com>
2022-02-03 23:19:50 -0500

    Testing get customer by id

--- services/CustomerService.js
+++ services/CustomerService.js
@@ -2,6 +2,7 @@ const customerModel = require("../models/CustomerModel.js");
 const bcrypt = require("bcryptjs");
 
 exports.createACustomer = async(req, res) =>{
+    // validation happen before this func in validation.js refer to CustomerController.js
     const newCustomer = new customerModel(req.body); 
     try{
         const salt = await bcrypt.genSalt(10);
@@ -10,7 +11,7 @@ exports.createACustomer = async(req, res) =>{
         try{
             const savedCustomer = await newCustomer.save();
             res.status(201).json({
-                message: `The customer was successfully created and stored in the Rest-Inn database`,
+                message: `The customer was successfully created and stored in the Rest-Inn database.`,
                 result: savedCustomer
             })
         }
@@ -22,13 +23,37 @@ exports.createACustomer = async(req, res) =>{
     }
     catch(err){
         res.status(500).json({
-            message: `There was an error encrypting the password`
+            message: `There was an error encrypting the password.`
         })
     }
 };
 
-exports.getACustomer = (req, res) =>{
-    res.json({
-        message : `This is a GET request with id ${req.params.id}`
-    })
+exports.getACustomer = async(req, res) =>{
+    // validation happen for right id format
+    try{
+        const customer = await customerModel.findById(req.params.id);
+        if(customer){
+            res.json({
+                message: `Customer with id ${req.params.id}.`,
+                result: customer
+            })
+        }
+        else{
+            res.status(404).json({
+                message: `There are no customer with id ${req.params.id} in the Rest-Inn database.`
+            })
+        }
+    }
+    catch(err){
+        if (err.name === "CastError" && err.kind === "ObjectId") {
+            res.status(404).json({
+                message: `There is no customer in our database with id ${req.params.id}`
+            })
+        }
+        else {
+            res.status(500).json({
+                message: err
+            })
+        }
+    }
 };
