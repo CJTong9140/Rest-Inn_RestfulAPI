3fdc2eaf720a9a3ff6e914c85ebc6df962c2fe25
CJ Jingren Tong <cj.tong93@gmail.com>
2022-02-03 22:35:54 -0500

    Testing password encryption

--- services/CustomerService.js
+++ services/CustomerService.js
@@ -1,9 +1,30 @@
 const customerModel = require("../models/CustomerModel.js"); 
+const bcrypt = require("bcryptjs");
 
-exports.createACustomer = (req, res) =>{
-    res.json({
-        message : `This is a POST request`
-    })
+exports.createACustomer = async(req, res) =>{
+    const newCustomer = new customerModel(req.body); 
+    try{
+        const salt = await bcrypt.genSalt(10);
+        newCustomer.password = await bcrypt.hash(newCustomer.password, salt);
+
+        try{
+            const savedCustomer = await newCustomer.save();
+            res.status(201).json({
+                message: `The customer was successfully created and stored in the Rest-Inn database`,
+                result: savedCustomer
+            })
+        }
+        catch(error){
+            res.status(500).json({
+                message: error
+            })
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: `There was an error encrypting the password`
+        })
+    }
 };
 
 exports.getACustomer = (req, res) =>{
