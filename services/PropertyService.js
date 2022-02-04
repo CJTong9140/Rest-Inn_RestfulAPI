b25f344b824f8684efdee3bb00dc9a630a325f78
CJ Jingren Tong <cj.tong93@gmail.com>
2022-02-04 11:18:38 -0500

    Setting up Property Service

---
+++ services/PropertyService.js
@@ -0,0 +1,32 @@
+const propertyModel = require("../models/PropertyModel.js"); 
+
+// Retrieve all the properties in the database
+// Retrieve all the properties in the database that belong to a specified type
+// Retrieve all the properties in the database that belong to a particular location
+// Retrieve all the properties in the database that marked as bestsellers as true
+exports.getAllProperties = (req, res) => {
+
+}; 
+
+// Retrieve all the properties types in the database
+exports.getAllPropertiesTypes = (req, res) => {
+
+}; 
+
+// Retrieve a specific property by id
+exports.getAProperty = (req, res) => {
+
+}; 
+
+// Update a property by id
+exports.updateAProperty = (req, res) => {
+
+}; 
+
+// Delete a property by id
+exports.deleteAProperty = (req, res)=>{
+
+}
+
+
+
