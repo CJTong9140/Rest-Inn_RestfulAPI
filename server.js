3eebe6ef9759965e97171e96d2c0b410de45a4e4
CJ Jingren Tong <cj.tong93@gmail.com>
2022-02-06 23:35:58 -0500

    Final commit

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 /*********************************************************************************
 * WEB422 Rest-Inn RESTful API Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including 3rd party web sites) or distributed to other students. 
 *
 * Name: CJ Jingren Tong     Student ID: 152464194    Date: February 6th, 2022
 * Online (Heroku) Link: 
